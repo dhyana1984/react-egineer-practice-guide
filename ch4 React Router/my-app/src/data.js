a56a0ab8796a07c5da3c2a7d3e89abd3b35dd116
yxiong <yxiong@fnz.com>
2021-02-16 23:46:24 +0800

    react router

---
+++ ch4 React Router/my-app/src/data.js
@@ -0,0 +1,27 @@
+let data = {
+    family: {
+        title: 'Family',
+        list: [
+            { name: 'Dad' },
+            { name: 'Mom' }
+        ]
+    },
+    friend: {
+        title: 'Friend',
+        list: [
+            { name: 'Tom' },
+            { name: 'Jack' },
+            { name: 'Lily' }
+        ]
+    },
+    customer: {
+        title: 'Client',
+        list: [
+            { name: 'Google' },
+            { name: 'Amazon' },
+            { name: 'Microsoft' }
+        ]
+    }
+};
+
+export default data;
