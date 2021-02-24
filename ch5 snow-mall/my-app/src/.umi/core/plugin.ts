f660a87df6b8a1d770e581612014830ab2ea4fe5
yxiong <yxiong@fnz.com>
2021-02-25 02:28:40 +0800

    Delete wrong folder

--- ch5 snow-mall/my-app/src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/yxiong/Repository/react-engineer-practice-guide/ch5 snow-mall/my-app/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','getInitialState','initialStateConfig','request',],
-});
-
-export { plugin };
--- ch5 snow-mall/my-app/src/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from '/Users/yxiong/Repository/react-engineer-practice-guide/ch5 snow-mall/my-app/node_modules/react-helmet';
